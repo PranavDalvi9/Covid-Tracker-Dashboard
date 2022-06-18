e7011cbd60f12929764b6ae5c0890f5c01d03913
PranavDalvi9 <96105012+PranavDalvi9@users.noreply.github.com>
2022-06-18 11:35:37 +0530

    database formated

--- server/src/configs/db.js
+++ server/src/configs/db.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
 module.exports = () => {
-  return mongoose.connect("mongodb+srv://pranav:pranav123@cluster0.gvulk.mongodb.net/covid-tracker-Data?retryWrites=true&w=majority");
+  return mongoose.connect(
+    "mongodb+srv://pranav:pranav123@cluster0.gvulk.mongodb.net/covid-tracker-Data?retryWrites=true&w=majority"
+  );
 };
