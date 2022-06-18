713f8a506aa2153fb215728def509276f5dc9080
PranavDalvi9 <96105012+PranavDalvi9@users.noreply.github.com>
2022-06-18 11:37:10 +0530

    controller updated

--- server/src/controllers/CovidController.js
+++ server/src/controllers/CovidController.js
@@ -14,30 +14,27 @@ router.get("/Countries", async (req, res) => {
     });
 });
 
-
 router.get("/Global", async (req, res) => {
-    const data = await axios
-      .get("https://api.covid19api.com/summary")
-      .then((ress) => {
-        return res.send(ress.data.Global);
-      })
-      .catch((err) => {
-        return res.send(err);
-      });
-  });
+  const data = await axios
+    .get("https://api.covid19api.com/summary")
+    .then((ress) => {
+      return res.send(ress.data.Global);
+    })
+    .catch((err) => {
+      return res.send(err);
+    });
+});
 
-  router.get("/individual/:id", async (req, res) => {
-    // console.log(req.params.id)
-    const data = await axios
-      .get(`https://api.covid19api.com/live/country/${req.params.id}`)
-      .then((ress) => {
-        return res.send(ress.data);
-      })
-      .catch((err) => {
-        return res.send(err);
-      });
-  });
-
+router.get("/individual/:id", async (req, res) => {
+  // console.log(req.params.id)
+  const data = await axios
+    .get(`https://api.covid19api.com/live/country/${req.params.id}`)
+    .then((ress) => {
+      return res.send(ress.data);
+    })
+    .catch((err) => {
+      return res.send(err);
+    });
+});
 
 module.exports = router;
-
